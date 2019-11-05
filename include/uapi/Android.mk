05013101e6c59b56cc37469340f1b23038281a61
Pavel Dubrova <pashadubrova@gmail.com>
2019-11-06 00:59:49 +0200

    uapi: Android.mk: Set absolute path of the top tree
    This will allow you to be independent of the path
    to the kernel sources.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- include/uapi/Android.mk
+++ include/uapi/Android.mk
@@ -10,7 +10,7 @@ AUDIO_KERNEL_HEADERS := $(call all-named-files-under,*.h,linux) $(call all-named
 
 HEADER_INSTALL_DIR := kernel/msm-$(TARGET_KERNEL_VERSION)/scripts
 
-BUILD_ROOT_RELATIVE := ../../../../../../../
+BUILD_ROOT := $(shell pwd)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := audio_kernel_headers
@@ -19,7 +19,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_PREBUILT_INT_KERNEL)
 GEN := $(addprefix $(UAPI_OUT)/,$(AUDIO_KERNEL_HEADERS))
 $(GEN): $(KERNEL_USR)
 $(GEN): PRIVATE_PATH := $(MYLOCAL_PATH)
-$(GEN): PRIVATE_CUSTOM_TOOL = $(shell cd $(PRODUCT_OUT)/obj/KERNEL_OBJ; $(BUILD_ROOT_RELATIVE)$(HEADER_INSTALL_DIR)/headers_install.sh $(BUILD_ROOT_RELATIVE)$(dir $@) $(BUILD_ROOT_RELATIVE)$(subst $(UAPI_OUT),$(MYLOCAL_PATH),$(dir $@)) $(notdir $@))
+$(GEN): PRIVATE_CUSTOM_TOOL = $(shell cd $(PRODUCT_OUT)/obj/KERNEL_OBJ; $(BUILD_ROOT)/$(HEADER_INSTALL_DIR)/headers_install.sh $(BUILD_ROOT)/$(dir $@) $(BUILD_ROOT)/$(subst $(UAPI_OUT),$(MYLOCAL_PATH),$(dir $@)) $(notdir $@))
 $(GEN): $(addprefix $(MYLOCAL_PATH)/,$(AUDIO_KERNEL_HEADERS))
 	$(transform-generated-source)
 
